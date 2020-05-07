9b4b0b152a9b9458c6b1f19f01d18c26ace94fa9
Jerry <1041075045@qq.com>
2020-05-07 13:49:51 +0800

    修改了部分代码，在输入的年龄不符合逻辑时，年龄会进行初始化

--- src/Date0506/Person1Test.java
+++ src/Date0506/Person1Test.java
@@ -6,6 +6,7 @@ public class Person1Test {
     public static void main(String[] args) {
         Person1 person = new Person1();
         for (int i = 0; ; ) {
+            person.setAge(0);
             Scanner sc = new Scanner(System.in);
             System.out.println("\n请输入年龄：");
             String a = sc.next();
@@ -15,7 +16,7 @@ public class Person1Test {
             } else {
                 int age = Integer.parseInt(a);
                 person.setAge(age);
-                if (person.getAge() > 0 && person.getAge() < 12 || person.getAge() > 65 && person.getAge() < 100) {
+                if (person.getAge() > 0 && person.getAge() < 12 || person.getAge() > 65 && person.getAge() <= 100) {
                     System.out.println("这个人" + person.getAge() + "岁了。");
                     System.out.println("这个人属于老人或者小孩");
                     System.out.println("这个人可以免费进入该景区。");
