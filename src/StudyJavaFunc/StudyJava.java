1e7b847c41c19c04ec491e12fd594fae481acc76
Jerry <1041075045@qq.com>
2020-05-11 08:41:49 +0800

    学习java白皮书遇到的一些注意点

---
+++ src/StudyJavaFunc/StudyJava.java
@@ -0,0 +1,26 @@
+package StudyJavaFunc;
+
+public class StudyJava {
+    public static void main(String[] args) {
+        /*一个正整数除以0的结果是正无穷大，计算0/0或者负数的平方根结果为NaN
+        double a = 0/0;
+        System.out.println(a);*/
+
+        /*浮点数值采用二进制系统表示无法精确地表示分数1/10
+        double a = 2.0-1.1;
+        System.out.println(a);*/
+
+        /*char类型中分别代表注册字符和π，在使用char类型字符的时候要注意\u的使用
+        char b = '\u03C0';
+        char a = '\u2122';
+        System.out.println(a);*/
+
+        /*变量名未赋值，编译不过会报错。
+        int a;
+        System.out.println(a);*/
+
+        /*使用final来指示常量，一旦被赋值之后，该变量不能再进行更改，习惯上常量名使用全大写
+        final int A = 10;
+        int A =20;*/
+    }
+}
